ec3b43372c892a7b64ec044ec04366f5a12d6f16
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2023-03-07 15:58:39 -0800

    Added comment in RoomObj Class

--- HotelRegistration/src/RoomObj.java
+++ HotelRegistration/src/RoomObj.java
@@ -1,4 +1,4 @@
 public class RoomObj {
-    //TODO: create a room object with object parameters int roomnumbers, double price, boolean pet allowed,
+    //create a room object with object parameters int roomnumbers, double price, boolean pet allowed,
     // int number of bed, Boolean issuite
 }
